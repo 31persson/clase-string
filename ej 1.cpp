dc395f232491d52f65fbfa0c3c2f5a5f97529270
31persson <jperscas@myuax.com>
2023-11-23 12:16:00 +0100

    ej 1 y 2

--- ej 1.cpp
+++ ej 1.cpp
@@ -2,11 +2,21 @@
 #include <string>
 
 int main() {
-    // Utilizar una constante para la frase
-    const std::string phrase = "Hello, World!";
+    // Crear una cadena utilizando la clase string
+    std::string phrase = "Hello, World!";
 
-    // Imprimir la frase en la consola
+    // Imprimir la cadena
     std::cout << "La frase es: " << phrase << std::endl;
+
+    // Imprimir el tamaño de la cadena utilizando la función size()
+    std::cout << "El tamaño de la cadena es: " << phrase.size() << " caracteres." << std::endl;
+
+    // Comprobar si la cadena está vacía utilizando la función empty()
+    if (phrase.empty()) {
+        std::cout << "La cadena esta vacia." << std::endl;
+    } else {
+        std::cout << "La cadena no está vacia." << std::endl;
+    }
 
     return 0;
 }
