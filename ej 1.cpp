f6e9baaa4841939996e4612aca92e39ca8b5cb03
31persson <jperscas@myuax.com>
2023-11-23 12:14:22 +0100

    ej 1

--- ej 1.cpp
+++ ej 1.cpp
@@ -2,15 +2,11 @@
 #include <string>
 
 int main() {
-    // Declarar una variable de tipo string para representar la frase
-    std::string frase;
-
-    // Pedir al usuario que ingrese una frase
-    std::cout << "Ingresa una frase: ";
-    std::getline(std::cin, frase);
+    // Utilizar una constante para la frase
+    const std::string phrase = "Hello, World!";
 
     // Imprimir la frase en la consola
-    std::cout << "La frase que ingresaste es: " << frase << std::endl;
+    std::cout << "La frase es: " << phrase << std::endl;
 
     return 0;
 }
